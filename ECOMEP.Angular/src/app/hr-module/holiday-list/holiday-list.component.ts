8cdb469c4fff2ec76e5509270d5fc15ca16a10e2
itecomep <it@ecomep.in>
2026-05-12 12:58:22 +0530

    current year holiday list

--- ECOMEP.Angular/src/app/hr-module/holiday-list/holiday-list.component.ts
+++ ECOMEP.Angular/src/app/hr-module/holiday-list/holiday-list.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HolidayMasterService } from '../../leave/services/holiday-master-api.service';
-import { Holiday } from '../../leave/models/holiday.model';
+import { HolidayMasterService } from "../../leave/services/holiday-master-api.service";
+import { Holiday } from "../../leave/models/holiday.model";
 
 @Component({
-  selector: 'app-holiday-list',
+  selector: "app-holiday-list",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './holiday-list.component.html',
-  styleUrls: ['./holiday-list.component.scss']
+  templateUrl: "./holiday-list.component.html",
+  styleUrls: ["./holiday-list.component.scss"],
 })
 export class HolidayListComponent implements OnInit {
-
   holidaysMasterArr: Holiday[] = [];
 
-  constructor(
-    private holidayService: HolidayMasterService
-  ) { }
+  constructor(private holidayService: HolidayMasterService) {}
 
   ngOnInit(): void {
     this.getHolidayData();
@@ -25,16 +22,16 @@ export class HolidayListComponent implements OnInit {
 
   getHolidayData() {
     this.holidayService.get().subscribe((data: Holiday[]) => {
+      const currentYear = new Date().getFullYear();
 
-      this.holidaysMasterArr = data;
+      this.holidaysMasterArr = data.filter((item) => {
+        return new Date(item.holidayDate).getFullYear() === currentYear;
+      });
 
       this.holidaysMasterArr.sort(
         (a, b) =>
-          new Date(a.holidayDate).getTime() -
-          new Date(b.holidayDate).getTime()
+          new Date(a.holidayDate).getTime() - new Date(b.holidayDate).getTime(),
       );
-
     });
   }
-
 }
